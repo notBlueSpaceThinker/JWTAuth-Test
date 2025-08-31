32ae8051f58125d3f39b750d8030cdc918c1ef01
Yuztrazure <artemijsidnev04923@gmail.com>
2025-08-31 12:03:48 +0300

    Добавлен маршрут /register, добавлена проверка на существование пользователя в  /login

--- app/endpoints/login.py
+++ app/endpoints/login.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Response, Request, HTTPException, status
 
 from app.auth import create_access_token, get_payload_from_token
-from app.database import auth_user
+from app.database import auth_user, add_user, get_user
 from app.schemas.user import User
 
 router = APIRouter()
@@ -9,6 +9,11 @@ router = APIRouter()
 
 @router.post("/login")
 async def login_user(user: User, response: Response):
+    if not get_user(user.username):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
     if auth_user(user.username, user.password):
         token = create_access_token({"sub": user.username})
 
@@ -26,6 +31,25 @@ async def login_user(user: User, response: Response):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Unathorized user"
         )
+    
+@router.post("/register", status_code=status.HTTP_201_CREATED)
+async def register_user(user: User, response: Response):
+    if get_user(user.username):
+        raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail="User already exisrs"
+    )
+    add_user(user.username, user.password)
+    token = create_access_token({"sub": user.username})
+
+    response.set_cookie(
+        key="access_token",
+        value=token,
+        httponly=True,
+        samesite="lax",
+        secure=False
+    )
+    return {"message": "New user created"}
     
 @router.get("/protected_resourse")
 async def protected_resourse(request: Request):
@@ -45,4 +69,4 @@ async def protected_resourse(request: Request):
             detail="Invalid token payload",
         )
     
-    return {"message": "Access granted"}
+    return {"message": f"{username}, access granted"}
